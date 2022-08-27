903c124eb041f06d60fb49fc451810ccfde6d2d2
arubiov <andressano911@hotmail.com>
2022-08-26 22:46:11 -0500

    clase empleado

---
+++ Project3/src/main/java/com/Ciclo3/Developers_CAR/Project3/_entity/Empleado.java
@@ -0,0 +1,43 @@
+package com.Ciclo3.Developers_CAR.Project3._entity;
+
+public class Empleado {
+    private String nombreEmpleado;
+    private String correoEmpleado;
+    private String empresaEmpleado;
+    private String rolEmpleado;
+
+    public Empleado() {
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(String empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+}
