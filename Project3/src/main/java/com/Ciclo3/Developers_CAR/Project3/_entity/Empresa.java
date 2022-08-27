a54341be7bd3a50e13ab949ce1c82411f81aebe7
eaperaza <111408076+eaperaza@users.noreply.github.com>
2022-08-26 21:51:27 -0500

    creacion clase empresa, eder

---
+++ Project3/src/main/java/com/Ciclo3/Developers_CAR/Project3/_entity/Empresa.java
@@ -0,0 +1,43 @@
+package com.Ciclo3.Developers_CAR.Project3._entity;
+
+public class Empresa {
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private String telefonoEmpresa;
+    private String nitEmpresa;
+
+    public Empresa() {
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public String getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(String telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public String getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(String nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+}
