978cbc6abdfe3b5b7c0322294313ec2b3d5bb678
arubiov <andressano911@hotmail.com>
2022-09-04 21:10:51 -0500

    restcontrollerMovimientoDinero , iMovimientoDineroService

--- Project3/src/main/java/com/Ciclo3/Developers_CAR/Project3/controller/MovimientoDineroRestController.java
+++ Project3/src/main/java/com/Ciclo3/Developers_CAR/Project3/controller/MovimientoDineroRestController.java
@@ -1,5 +1,36 @@
 package com.Ciclo3.Developers_CAR.Project3.controller;
+import com.Ciclo3.Developers_CAR.Project3.entity.Empresa;
+import com.Ciclo3.Developers_CAR.Project3.entity.MovimientoDinero;
+import com.Ciclo3.Developers_CAR.Project3.service.IMovimientoDineroService;
+import com.Ciclo3.Developers_CAR.Project3.service.iEmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 public class MovimientoDineroRestController {
+    @Autowired
+
+    private IMovimientoDineroService movimientoDineroService;
+
+    @GetMapping("/rol/{id}")
+    public MovimientoDinero FindById(@PathVariable int id) {
+        return movimientoDineroService.FindById(id);
+    }
+    @GetMapping("/rol")
+    public List<MovimientoDinero> FindAll(){
+        return movimientoDineroService.FindAll();
+    }
+    @PostMapping("/rol")
+    public MovimientoDinero createdMovimientoDinero(@RequestBody MovimientoDinero movimiento){
+        return movimientoDineroService.createdMovimientoDinero(movimiento);
+    }
+    @PutMapping("/rol/{id}")
+    public MovimientoDinero UpdateMovimientoDinero(@PathVariable int id , @RequestBody MovimientoDinero movimiento){
+        return movimientoDineroService.UpdateMovimientoDinero(id,movimiento);
+    }
+    @DeleteMapping("/rol/{id}")
+    public void deleteMovimientoDinero(@PathVariable int id){
+        movimientoDineroService.deleteMovimientoDinero(id);
+    }
 }
